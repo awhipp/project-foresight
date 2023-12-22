dcfe8f16e9557e517c0b4e5f6a3ebdce68b72f1b
Alexander Whipp <alexruns4@gmail.com>
2023-12-22 00:14:39 -0500

    Initial refactor for the steam service
    Still need to create the model folder and add a pydantic model for the steaming forex data

--- stream_service/app.py
+++ stream_service/app.py
@@ -1,33 +1,25 @@
-'''Streaming service for getting FOREX data to a data store.'''
-import sys
-from pathlib import Path
+"""Streaming service for getting FOREX data to a data store."""
 import json
 import os
 
 import requests
 
 import dotenv
+
 dotenv.load_dotenv(".env")
 
 # Setup logging and log timestamp prepend
-import logging
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s', 
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
-logger = logging.getLogger(__name__)
+from utils.logger import generate_logger
 
-# Determine fixed path for imports
-sys.path.insert(0, str(Path(__file__).parent.parent))
+logger = generate_logger(__name__)
 from utils.database import TimeScaleService
 
+
 def open_stream():
-    '''Open a stream to the OANDA API and send the data to the data store.
-    '''
-    account_id = os.getenv('OANDA_API_ACCOUNT_ID')
-    api_token = os.getenv('OANDA_API_TOKEN')
-    random_walk = os.getenv('APP_RANDOM_WALK', 'False') == 'True'
+    """Open a stream to the OANDA API and send the data to the data store."""
+    account_id = os.getenv("OANDA_API_ACCOUNT_ID")
+    api_token = os.getenv("OANDA_API_TOKEN")
+    random_walk = os.getenv("APP_RANDOM_WALK", "False") == "True"
 
     if random_walk:
         from random import random
@@ -39,16 +31,21 @@ def open_stream():
         while True:
             initial_price = initial_price * (1.0 + (random() - 0.5) * 0.1)
             record = {
-                'instrument': 'EUR_USD',
-                'time': datetime.now().isoformat(),
-                'bid': round(initial_price, 5),
-                'ask': round(initial_price + 0.0001, 5)
+                "instrument": "EUR_USD",
+                "time": datetime.now().isoformat(),
+                "bid": round(initial_price, 5),
+                "ask": round(initial_price + 0.0001, 5),
             }
-            
+
             TimeScaleService().execute(
                 query="""INSERT INTO forex_data (instrument, time, bid, ask)
                 VALUES (%s, %s, %s, %s)""",
-                params=(record['instrument'], record['time'], record['bid'], record['ask'])
+                params=(
+                    record["instrument"],
+                    record["time"],
+                    record["bid"],
+                    record["ask"],
+                ),
             )
 
             logger.info(record)
@@ -56,35 +53,41 @@ def open_stream():
             sleep(5)
 
     else:
-        url = f'https://stream-fxtrade.oanda.com/v3/accounts/{account_id}/pricing/stream?instruments=EUR_USD'
+        url = f"https://stream-fxtrade.oanda.com/v3/accounts/{account_id}/pricing/stream?instruments=EUR_USD"
         head = {
-            'Content-type':"application/json",
-            'Accept-Datetime-Format':"RFC3339",
-            'Authorization':f"Bearer {api_token}"
+            "Content-type": "application/json",
+            "Accept-Datetime-Format": "RFC3339",
+            "Authorization": f"Bearer {api_token}",
         }
         resp = requests.get(url, headers=head, stream=True, timeout=30).iter_lines()
         for line in resp:
             if line:
-                decoded_line = line.decode('utf-8')
+                decoded_line = line.decode("utf-8")
                 obj = json.loads(decoded_line)
-                if obj['type'] == 'PRICE' and obj['tradeable']:
+                if obj["type"] == "PRICE" and obj["tradeable"]:
                     record = {
-                        'instrument': obj['instrument'],
-                        'time': obj['time'],
-                        'bid': float(obj['bids'][0]['price']),
-                        'ask': float(obj['asks'][0]['price'])
+                        "instrument": obj["instrument"],
+                        "time": obj["time"],
+                        "bid": float(obj["bids"][0]["price"]),
+                        "ask": float(obj["asks"][0]["price"]),
                     }
-                    
+
                     TimeScaleService().execute(
                         query="""INSERT INTO forex_data (instrument, time, bid, ask)
                         VALUES (%s, %s, %s, %s)""",
-                        params=(record['instrument'], record['time'], record['bid'], record['ask'])
+                        params=(
+                            record["instrument"],
+                            record["time"],
+                            record["bid"],
+                            record["ask"],
+                        ),
                     )
 
                     logger.info(record)
 
+
 def create_table():
-    '''Create a table in the data store.'''
+    """Create a table in the data store."""
 
     # Execute SQL queries here
     TimeScaleService().create_table(
@@ -94,12 +97,13 @@ def create_table():
         bid FLOAT NOT NULL,
         ask FLOAT NOT NULL,
         PRIMARY KEY (instrument, time)                   
-    )""",  
-        hyper_table_name='forex_data',
-        hyper_table_column='time'
+    )""",
+        hyper_table_name="forex_data",
+        hyper_table_column="time",
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Execute SQL queries here
     TimeScaleService().create_table(
         query="""CREATE TABLE IF NOT EXISTS forex_data (
@@ -108,15 +112,15 @@ if __name__ == '__main__':
         bid FLOAT NOT NULL,
         ask FLOAT NOT NULL,
         PRIMARY KEY (instrument, time)                   
-    )""",  
-        hyper_table_name='forex_data',
-        hyper_table_column='time'
+    )""",
+        hyper_table_name="forex_data",
+        hyper_table_column="time",
     )
-    
+
     # Open a stream to the OANDA API and send the data to the data store.
     while True:
         try:
             open_stream()
         except Exception as e:
             logger.error(e)
-            logger.info('Restarting stream...')
+            logger.info("Restarting stream...")
